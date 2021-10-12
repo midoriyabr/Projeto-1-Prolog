db04aec7c31c8726814cefa6bc2848b22a471183
midoriyabr <monojaxmain@gmail.com>
2021-10-12 11:30:21 -0300

    Iniciando o projeto

---
+++ bot-pc.pl
@@ -0,0 +1,20 @@
+marca(intel, i3_3240).
+marca(intel, i5_3330).
+marca(intel, i7_3770).
+marca(intel, i3_9100).
+marca(intel, i5_9600KF).
+marca(intel, i7_7700K).
+
+socket(fclga1155, i3_3240).
+socket(fclga1155, i5_3330).
+socket(fclga1155, i7_3770).
+socket(fclga1151, i3_9100).
+socket(fclga1151, i5_9600KF).
+socket(fclga1151, i7_7700K).
+
+valor(i3_3240, 239.90).
+valor(i5_3330, 359.00).
+valor(i7_3770, 790.01).
+valor(i3_9100, 1091.85).
+valor(i5_9600KF, 1538.99).
+valor(i7_7700K, 1861.48).
